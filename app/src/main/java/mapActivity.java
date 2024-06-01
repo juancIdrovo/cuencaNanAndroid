45103cb57e4716af30a0286f2e307ad0d5742106
Juanc Idrovo <juancidrovo@gmail.com>
2024-06-01 11:14:05 -0500

    Carga inicial del proyecto

---
+++ app/src/main/java/mapActivity.java
@@ -0,0 +1,17 @@
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class mapActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
+
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
