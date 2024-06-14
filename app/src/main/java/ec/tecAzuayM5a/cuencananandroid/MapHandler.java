2eb44101bde5853841de378aa433583021b632bb
Juanc Idrovo <juancidrovo@gmail.com>
2024-06-14 09:02:32 -0500

    cambios para mapa
    algunos cambios para el mapa

--- app/src/main/java/ec/tecAzuayM5a/cuencananandroid/MapHandler.java
+++ app/src/main/java/ec/tecAzuayM5a/cuencananandroid/MapHandler.java
@@ -1,34 +1,42 @@
 package ec.tecAzuayM5a.cuencananandroid;
 
-import androidx.annotation.NonNull;
-import com.google.android.gms.maps.CameraUpdateFactory;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.annotation.NonNull;
 
-public class MapHandler implements OnMapReadyCallback {
+public class MapHandler extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap myMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map); // Apunta al layout correcto que contiene el mapa
+
+        // Encuentra el fragmento del mapa y pásaselo a esta Activity
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         myMap = googleMap;
 
-
         LatLng cuenca = new LatLng(-2.9001285, -79.00589649999999);
 
-        //Marcador
+        // Marcador
         myMap.addMarker(new MarkerOptions().position(cuenca).title("Cuenca"));
 
-
-        //zoom
+        // Zoom
         float zoomLevel = 15.0f;
         myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cuenca, zoomLevel));
-    }
-
-    public void initializeMap(SupportMapFragment mapFragment) {
-        mapFragment.getMapAsync(this);
     }
 }
