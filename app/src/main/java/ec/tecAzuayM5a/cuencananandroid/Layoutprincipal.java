5ad41042a7e17599e2f527586767769f672548eb
Juanc Idrovo <juancidrovo@gmail.com>
2024-07-02 09:44:14 -0500

    gg
    para fragment

--- app/src/main/java/ec/tecAzuayM5a/cuencananandroid/Layoutprincipal.java
+++ app/src/main/java/ec/tecAzuayM5a/cuencananandroid/Layoutprincipal.java
@@ -121,7 +121,7 @@ public class Layoutprincipal extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_fragment_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_puntosInteres)
+                R.id.nav_fragment_home, R.id.nav_gallery, R.id.puntosInteres, R.id.nav_puntosInteres, R.id.calificarPuntoInteres)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_layoutprincipal);
