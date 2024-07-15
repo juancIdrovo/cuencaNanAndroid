2337ed9dcf18621f20db8b8b87d00b702be2e831
Juanc Idrovo <juancidrovo@gmail.com>
2024-07-15 09:17:37 -0500

    gg
    gg

--- app/src/main/java/ec/tecAzuayM5a/cuencananandroid/ip/ip.java
+++ app/src/main/java/ec/tecAzuayM5a/cuencananandroid/ip/ip.java
@@ -1,7 +1,7 @@
 package ec.tecAzuayM5a.cuencananandroid.ip;
 
 public class ip {
-    public String ip = "http://192.168.1.25:8080/api";
+    public String ip = "http://192.168.100.196:8080/api";
 
     public ip() {
     }
